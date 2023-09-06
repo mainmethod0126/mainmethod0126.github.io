18e9c852d0b43837f761f769a9d4ab0e442a789e
wusub.shin@softcamp.co.kr <wusub.shin@softcamp.co.kr>
2023-09-06 13:01:51 +0900

    블로그 이전 작업 중

--- theme.config.tsx
+++ theme.config.tsx
@@ -39,7 +39,22 @@ const config: DocsThemeConfig = {
       if (title === 'common') {
         return <>📒 {title}</>
       }
+      if (title === 'linux') {
+        return <>📒 {title}</>
+      }
       if (title === 'java') {
+        return <>📒 {title}</>
+      }
+      if (title === 'nginx') {
+        return <>📒 {title}</>
+      }
+      if (title === 'security') {
+        return <>📒 {title}</>
+      }
+      if (title === 'vue') {
+        return <>📒 {title}</>
+      }
+      if (title === 'kafka') {
         return <>📒 {title}</>
       }
       if (title === 'serialized data structure') {
